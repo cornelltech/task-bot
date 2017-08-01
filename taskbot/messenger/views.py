10b2aec387b2d000cfcce97cf8a556abe2c0d3ee
Adrian Vatchinsky <avatchinsky@outlook.com>
2017-08-01 14:18:06 -0400

    updated the basic pipeline

--- taskbot/messenger/views.py
+++ taskbot/messenger/views.py
@@ -4,8 +4,69 @@ from __future__ import unicode_literals
 import json
 import os
 
+import requests
 from django.http import HttpResponse, JsonResponse
 from django.views import View
+
+
+def receive_message(event):
+    sender_id       = event.get('sender').get('id')
+    recipient_id    = event.get('recipient').get('id')
+    time_of_message = event.get('timestamp')
+    message         = event.get('message')
+
+    print( 'Received message for user {0} and page {1} at {2} with message:'.format(sender_id, recipient_id, time_of_message) )
+
+    message_id          = message.get('mid')
+    message_text        = message.get('text')
+    message_attachments = message.get('attachments')
+
+    if message_text:
+        if message_text == 'hi':
+            send_generic_message(sender_id)
+        else:
+            send_text_message(sender_id, message_text)
+    elif message_attachments:
+        send_text_message(sender_id, 'received attachments')
+
+
+def send_generic_message(recipient_id, message_text):
+    print "send message"
+
+
+def send_text_message(recipient_id, message_text):
+    message_data = {
+        'recipient': {
+            'id': recipient_id
+        },
+        'message': {
+            'text': message_text
+        }
+    }
+
+    call_send_api(message_data)
+
+
+def call_send_api(message_data): 
+    req = requests.post(
+        'https://graph.facebook.com/v2.6/me/messages',
+        params = os.environ.get('FB_ACCESS_TOKEN', None),
+        data = json.dumps( message_data )
+    )
+    
+    # res = json.loads( req.json() )
+    print req.json()
+
+    # if req.status_code == 200:
+    #     print res
+    #     recipient_id = res.get('message_id')
+    #     message_id = res.get('message_id')
+
+    #     print( 'Successfully sent generic message with id {0} to recipient {1}'.format(recipient_id, message_id) )
+
+    # else:
+    #     print("Unable to send message")
+    #     print( res.json() )
 
 
 # Create your views here.
@@ -13,27 +74,40 @@ class FBWebhookResponseView(View):
     def get(self, request, *args, **kwargs):
         TOKEN = os.environ.get('FB_CHALLENGE', None)
 
-        requestUrlParams = request.GET
-        if requestUrlParams.get('hub.mode', None) == 'subscribe' and requestUrlParams.get('hub.verify_token', None) == TOKEN:
-            challenge = requestUrlParams.get('hub.challenge')
+        request_url_params = request.GET
+        if request_url_params.get('hub.mode', None) == 'subscribe' and request_url_params.get('hub.verify_token', None) == TOKEN:
+            challenge = request_url_params.get('hub.challenge')
             return HttpResponse( challenge, status=200 )
         else:
             return HttpResponse( status=403 )
 
     def post(self, request, *args, **kwargs):
 
-        requestBody = json.loads(request.body)
+        request_body = json.loads(request.body)
 
-        print requestBody
+        try:
+            if request_body.get('object') == 'page':
+            
+                #  Make sure this is a page subscription
+                entry = request_body.get('entry')
+                
+                # Iterate over each entry - there may be multiple if batched
+                for obj in entry:
+                    page_id = obj.get('id')
+                    time_of_event = obj.get('time')
 
-        # if (req.query['hub.mode'] === 'subscribe' &&
-        #     req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
-        #     console.log("Validating webhook");
-        #     res.status(200).send(req.query['hub.challenge']);
-        # } else {
-        #     console.error("Failed validation. Make sure the validation tokens match.");
-        #     res.sendStatus(403);          
-        # }  
+                    # Iterate over each messaging event
+                    messaging = obj.get('messaging')
+                    for msg in messaging:
+                        if msg.get('message'):
+                            receive_message(msg)
+                        else:
+                            print( 'Webhook received unknown event: {0}'.format(event) )
+            else:
+                pass
+        except Exception as e:
+            print(e)
+        
 
         response = JsonResponse({}, status=200)
         return response
--- taskbot/taskbot/settings.py
+++ taskbot/taskbot/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts.apps.AccountsConfig',
     'messenger.apps.MessengerConfig',
 ]
 
